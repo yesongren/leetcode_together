f2502f281d1f9820833de6165d214effb344df31
yesongren <youshouninn@gmail.com>
2020-02-23 02:30:40 +0800

    accepted solution #160. Intersection of Two Linked Lists

---
+++ src/songren/LC160_IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,46 @@
+package songren;
+
+public class LC160_IntersectionOfTwoLinkedLists {
+    /**
+     * 160. Intersection of Two Linked Lists
+     * Cycle Lists
+     */
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode last = headB;
+        while (last.next != null) {
+            last = last.next;
+        }
+        last.next = headB;
+
+        ListNode fast = headA;
+        ListNode slow = headA;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = headA;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                last.next = null;
+                return fast;
+            }
+        }
+        last.next = null;
+        return null;
+    }
+}
