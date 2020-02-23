c4a9cf4511fd99dc798bc62f716e6968405f8ba3
Di-Gu <isgudi00@gmail.com>
2020-02-22 19:32:37 -0800

    LC 11 using two pointer

---
+++ src/didi/Array/LC11_ContainerWithMostWater.java
@@ -0,0 +1,14 @@
+package didi;
+
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length-1, maxArea = 0, tmp = 0; // bug 1
+        while (left != right){
+            tmp = Math.min(height[left], height[right]) * (right - left);
+            if (tmp > maxArea) maxArea = tmp;
+            if (height[left] > height[right]) right--;
+            else left++;
+        }
+        return maxArea;
+    }
+}
