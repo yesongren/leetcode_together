a83506a1cbdd30cdc5d2425d43b17367c294d71c
Di-Gu <isgudi00@gmail.com>
2020-02-22 19:04:21 -0800

    Array 1-5

---
+++ src/didi/Array/LC1_TwoSum.java
@@ -0,0 +1,19 @@
+package didi;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++){
+            int rest = target - nums[i];
+            if (map.containsKey(rest)){
+                result[1] = i;
+                result[0] = map.get(rest);
+                return result;
+            }
+            else
+                map.put(nums[i],i);
+        }
+        return result;
+    }
+}
