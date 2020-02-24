2d8808ff90aa6d87e6e9efe45a587a6948c2e807
Di-Gu <isgudi00@gmail.com>
2020-02-23 16:50:09 -0800

    LC 63 UniquePath with obstacles

---
+++ src/didi/DynamicProgramming/LC063_UniquePaths2.java
@@ -0,0 +1,35 @@
+package didi.dp;
+
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid[0].length;
+        int n = obstacleGrid.length;
+        if(obstacleGrid.length == 0) return 0;
+        int[][] dp = new int[n][m];
+        for (int i = 0; i < m; i++){
+            if (obstacleGrid[0][i] == 1){
+                while (i<m){
+                    dp[0][i] = 0;
+                    i++;
+                }
+            }
+            else dp[0][i] = 1;
+        }
+        for (int i = 0; i < n; i++){
+            if (obstacleGrid[i][0] == 1){
+                while (i<n){
+                    dp[i][0] = 0;
+                    i++;
+                }
+            }
+            else dp[i][0] = 1;
+        }
+        for (int i = 1; i < n; i++){
+            for (int j = 1; j < m; j++){
+                if (obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                else dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[n-1][m-1];
+    }
+}
