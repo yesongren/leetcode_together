71f08fa88cd5a3b12ff40d76a6d6a3c1bd9770f6
Di-Gu <isgudi00@gmail.com>
2020-02-23 19:44:02 -0800

    LC 53 maximum subarray

--- src/didi/DynamicProgramming/LC053_MaximumSubarray.java
+++ src/didi/DynamicProgramming/LC053_MaximumSubarray.java
@@ -1,2 +1,33 @@
 package didi.dp;
 
+class Solution {
+    // Brute Force
+    public int maxSubArray1(int[] nums) {
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++){
+            int left = i;
+            int sum = 0;
+
+            while (left < nums.length){
+                sum += nums[left];
+                max = Math.max(sum,max);
+                left++;
+            }
+        }
+
+        return max;
+    }
+    // DP
+    public int maxSubArray(int[] nums){
+        int[] dp = new int[nums.length];
+        int max = nums[0];
+        dp[0] = max;
+
+        for (int i = 1; i < nums.length; i++){
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            max = Math.max(dp[i], max);
+        }
+        return max;
+    }
+}
