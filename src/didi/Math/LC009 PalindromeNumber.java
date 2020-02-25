47b8257a4eb954a79466c5e2a076bbb5df6a7248
Di-Gu <isgudi00@gmail.com>
2020-02-25 00:40:32 -0800

    LC 9 pop digit the sum

--- src/didi/Math/LC009 PalindromeNumber.java
+++ src/didi/Math/LC009 PalindromeNumber.java
@@ -1,2 +1,20 @@
 package didi.math;
 
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0) return false;
+
+        return x == reverse(x);
+    }
+
+    public int reverse(int num){
+        int rst = 0;
+
+        while (num != 0) {
+            rst = rst * 10 + num % 10;
+            num = num / 10;
+        }
+
+        return rst;
+    }
+}
