e1f658ba0597dda6002356e718ccd620fc819730
Di-Gu <isgudi00@gmail.com>
2020-02-24 23:39:59 -0800

    LC 7 Reverse Integer

--- src/didi/Math/LC007_ReverseInteger.java
+++ src/didi/Math/LC007_ReverseInteger.java
@@ -1,2 +1,24 @@
 package didi.math;
 
+class Solution {
+    public int reverse(int x) {
+        String xString = Integer.toString(x);
+        int idx = (x > 0)? 0 : 1;
+        int sum = 0;
+        int len = xString.length()-idx;
+        int power = 0;
+
+        for (int i = idx; i < xString.length(); i++){
+            if (power > len) break;
+            char c = xString.charAt(i);
+            sum += Character.getNumericValue(c) * Math.pow(10,power);
+            power++;
+        }
+
+        if (sum > Integer.MAX_VALUE - 1 || sum < Integer.MIN_VALUE) sum = 0;
+
+        if (idx == 1) sum = -sum;
+
+        return sum;
+    }
+}
