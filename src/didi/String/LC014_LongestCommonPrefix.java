42750688836d339de4413250ea9c7c70a5c98d3f
Di-Gu <isgudi00@gmail.com>
2020-02-25 21:43:48 -0800

    LC 14 divide and conquer (use mid)

--- src/didi/String/LC014_LongestCommonPrefix.java
+++ src/didi/String/LC014_LongestCommonPrefix.java
@@ -1,2 +1,33 @@
 package didi.String;
 
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs == null || strs.length == 0) return "";
+
+        return longestCommonPrefix(strs, 0, strs.length - 1);
+    }
+
+    private String longestCommonPrefix(String[] strs, int left, int right) {
+        if (left == right) return strs[left];
+        else {
+            int mid = (left + right) / 2;
+            String leftLCP = longestCommonPrefix(strs, left, mid);
+            String rightLCP = longestCommonPrefix(strs, mid + 1, right);
+            return commonPrefix(leftLCP, rightLCP);
+        }
+    }
+
+    private String commonPrefix(String left, String right) {
+        int lenMin = Math.min(left.length(), right.length()); // length for String not String[]
+
+        for (int i = 0; i < lenMin; i++) {
+            if (left.charAt(i) != right.charAt(i)) {
+                return left.substring(0, i);
+            }else{
+                continue;
+            }
+        }
+
+        return left.substring(0, lenMin);
+    }
+}
