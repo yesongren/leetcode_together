d887b0b63207ea4059ac68d53a6dfdb4d27d2e8e
yesongren <youshouninn@gmail.com>
2020-02-28 02:43:57 +0800

    AC solution #100. Same Tree

---
+++ src/songren/Tree/LC100_SameTree.java
@@ -0,0 +1,14 @@
+package songren.Tree;
+
+/**
+ * Created by yesongren on 2020/2/28
+ * 100. Same Tree
+ */
+public class LC100_SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val == q.val) return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return false;
+    }
+}
