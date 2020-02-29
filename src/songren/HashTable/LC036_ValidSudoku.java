f444685a1cbe4b6cf9605303dfa82acefe4c83d3
yesongren <youshouninn@gmail.com>
2020-02-29 23:21:57 +0800

    AC solution #036. Valid Sudoku

---
+++ src/songren/HashTable/LC036_ValidSudoku.java
@@ -0,0 +1,29 @@
+package songren.HashTable;
+
+/**
+ * Created by yesongren on 2020/2/29
+ * 36. Valid Sudoku
+ */
+public class LC036_ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] block = new boolean[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    int blockIndex = i / 3 * 3 + j / 3;
+                    if (row[i][num] || col[j][num] || block[blockIndex][num]) {
+                        return false;
+                    } else {
+                        row[i][num] = true;
+                        col[j][num] = true;
+                        block[blockIndex][num] = true;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
