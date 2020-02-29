030bae98875b26f740c752e8531a3371cbc7f6a6
Di-Gu <isgudi00@gmail.com>
2020-02-28 21:30:06 -0800

    LC 30 valid sudoku (HashSet with String, add() check)

---
+++ src/didi/HashTable/LC030_ValidSudoku.java
@@ -0,0 +1,20 @@
+package didi.HashTable;
+import java.util.*;
+
+class Solution {
+    // Deal with unique use HashSet
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.')
+                    if(!seen.add(board[i][j] + "seen in row " + i) || //use [i][j]
+                            !seen.add(board[i][j] + "seen in column" + j) ||
+                            !seen.add(board[i][j] + "seen in boxes" + i / 3 + "-" + j / 3)) return false;
+            }
+        }
+
+        return true;
+    }
+}
