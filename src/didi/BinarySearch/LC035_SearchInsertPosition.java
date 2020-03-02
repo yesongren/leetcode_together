672fb51596d28eb51107e4fc7cc1afd7d5ef6f73
Di-Gu <isgudi00@gmail.com>
2020-03-01 19:48:32 -0800

    LC 35 search insert position (binary search)

--- src/didi/BinarySearch/LC035_SearchInsertPosition.java
+++ src/didi/BinarySearch/LC035_SearchInsertPosition.java
@@ -1,1 +1,23 @@
-package didi.BinarySearch
+package didi.BinarySearch;
+
+class Solution {
+    public int searchInsert1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) return i;
+            if (nums[i] > target) return i;
+        }
+        return nums.length;
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length;
+
+        while(low < high) {
+            int mid = low + (high - low) / 2; //
+            if (nums[mid] < target) low = mid + 1;
+            else high = mid;
+        }
+
+        return low;
+    }
+}
