21f02a91dc028a1bba76d77d731bbf34e53fbb55
Di-Gu <isgudi00@gmail.com>
2020-03-01 19:48:00 -0800

    LC 33 search rotated array (find pivot then binary search)

--- src/didi/BinarySearch/LC033_SearchinRotatedSortedArray.java
+++ src/didi/BinarySearch/LC033_SearchinRotatedSortedArray.java
@@ -1,23 +1,35 @@
 package didi.BinarySearch;
 
 class Solution {
-    public int searchInsert1(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) return i;
-            if (nums[i] > target) return i;
-        }
-        return nums.length;
-    }
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
 
-    public int searchInsert(int[] nums, int target) {
-        int low = 0, high = nums.length;
+        int left = 0;
+        int right = nums.length - 1;
 
-        while(low < high) {
-            int mid = low + (high - low) / 2; //
-            if (nums[mid] < target) low = mid + 1;
-            else high = mid;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) left = mid + 1;
+            else right = mid;
         }
 
-        return low;
+        int start = left;
+        left = 0;
+        right = nums.length - 1;
+
+        if (nums[start] <= target && nums[right] >= target) {
+            left = start;
+        } else {
+            right = start;
+        }
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) left = mid + 1; //
+            else right = mid - 1; //
+        }
+
+        return -1;
     }
 }
