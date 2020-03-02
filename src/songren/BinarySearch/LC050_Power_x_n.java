c81f33802aadf11a36370200883f45d1ffa97b72
yesongren <youshouninn@gmail.com>
2020-03-03 03:12:43 +0800

    AC solution #050. Pow(x, n)

---
+++ src/songren/BinarySearch/LC050_Power_x_n.java
@@ -0,0 +1,18 @@
+package songren.BinarySearch;
+
+/**
+ * Created by yesongren on 2020/3/3
+ */
+public class LC050_Power_x_n {
+    public double divideCleanSolution(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            return 1 / divideCleanSolution(x, -n);
+        }
+        return n % 2 == 1
+                ? x * divideCleanSolution(x, n - 1)
+                : divideCleanSolution(x * x, n / 2);
+    }
+}
