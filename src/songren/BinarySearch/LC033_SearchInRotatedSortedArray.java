58936a7ab2604c7a61226abda3a81385c944188a
yesongren <youshouninn@gmail.com>
2020-03-03 01:11:21 +0800

    AC solution #033. Search in Rotated Sorted Array

---
+++ src/songren/BinarySearch/LC033_SearchInRotatedSortedArray.java
@@ -0,0 +1,36 @@
+package songren.BinarySearch;
+
+/**
+ * Created by yesongren on 2020/3/3
+ * 33. Search in Rotated Sorted Array
+ */
+public class LC033_SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[start] <= nums[mid]) {
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target <= nums[end] && target > nums[mid]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
