f7c6085c4e61e24d3b94fe01e80feb032748be42
Di-Gu <isgudi00@gmail.com>
2020-03-01 20:31:28 -0800

    LC 34 find fist last index

--- src/didi/BinarySearch/LC034_FindFirstandLastPositionofElementinSortedArray.java
+++ src/didi/BinarySearch/LC034_FindFirstandLastPositionofElementinSortedArray.java
@@ -1,1 +1,32 @@
-package didi.BinarySearch
+package didi.BinarySearch;
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = new int[] {-1,-1};
+        if (nums.length == 0 || nums == null) return result;
+
+        int leftIndex = -1, rightIndex = -1;
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) leftIndex = mid; // sepreate
+            if (nums[mid] >= target) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        left = 0; right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) rightIndex = mid; //
+            if (nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        result[0] = leftIndex;
+        result[1] = rightIndex;
+
+        return result;
+    }
+}
