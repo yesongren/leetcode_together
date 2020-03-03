e498a1a77619ae324b4eeedf41a0297d45840f07
Di-Gu <isgudi00@gmail.com>
2020-03-03 14:48:10 -0800

    LC 102 level order traversal

--- src/didi/BreadthFirstSearch/LC102_BinaryTreeLevelOrderTraversal.java
+++ src/didi/BreadthFirstSearch/LC102_BinaryTreeLevelOrderTraversal.java
@@ -1,1 +1,35 @@
 package didi.bfs;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            List<Integer> subList = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                TreeNode curr = queue.poll();
+                if (curr.left != null) queue.offer(curr.left);
+                if (curr.right != null) queue.offer(curr.right);
+                subList.add(curr.val);
+            }
+            result.add(subList);
+        }
+
+        return result;
+    }
+}
