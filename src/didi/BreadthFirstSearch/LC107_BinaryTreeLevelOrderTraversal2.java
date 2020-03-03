5d98a64dafa56bf36844928c3ca4874963e4b4ac
Di-Gu <isgudi00@gmail.com>
2020-03-03 14:22:39 -0800

    LC 107 level traversal 2

--- src/didi/BreadthFirstSearch/LC107_BinaryTreeLevelOrderTraversal2.java
+++ src/didi/BreadthFirstSearch/LC107_BinaryTreeLevelOrderTraversal2.java
@@ -11,6 +11,25 @@ package didi.bfs;
  */
 class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
 
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            List<Integer> subList = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                TreeNode curr = queue.poll();
+                if (curr.left != null) queue.offer(curr.left);
+                if (curr.right != null) queue.offer(curr.right);
+                subList.add(curr.val);
+            }
+            result.add(0, subList);
+        }
+
+        return result;
     }
 }
