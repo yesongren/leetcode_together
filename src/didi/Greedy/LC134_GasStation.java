43ec31f2da25f7ff0c13045a739184999a5d9604
Di-Gu <isgudi00@gmail.com>
2020-03-03 02:16:30 -0800

    LC 134 gas station

--- src/didi/Greedy/LC134_GasStation.java
+++ src/didi/Greedy/LC134_GasStation.java
@@ -1,6 +1,32 @@
 package didi.greedy;
 
 class Solution {
+    // Base Solution
+    public int canCompleteCircuit1(int[] gas, int[] cost) {
+        if (gas.length != cost.length) return -1;
+
+        int size = gas.length;
+
+        int[] station = new int[size*2];
+
+        for (int i = 0; i < size; i++) station[i] = gas[i] - cost[i];
+        for (int i = size; i < size * 2; i++) station[i] = station[i - size];
+
+        int sum = 0, index = -1;
+
+        for(int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++){
+                sum += station[i + j];
+                if (sum < 0) break;
+            }
+            if (sum < 0) {
+                sum = 0;
+            } else return i;
+        }
+
+        return -1;
+    }
+
     public int canCompleteCircuit(int[] gas, int[] cost) {
         if (gas.length != cost.length) return -1;
 
