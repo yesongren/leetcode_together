ba4535d26926a2311c5cbad8b241d2ab9fb4723d
Di-Gu <isgudi00@gmail.com>
2020-03-03 14:45:47 -0800

    LC 111 minimum depth (bfs)

--- src/didi/BreadthFirstSearch/LC111_MinimumDepthofBinaryTree.java
+++ src/didi/BreadthFirstSearch/LC111_MinimumDepthofBinaryTree.java
@@ -1,1 +1,35 @@
 package didi.bfs;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int result = 1;
+
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode curr = queue.poll();
+                if (curr.left == null && curr.right == null)
+                    return result;
+                if (curr.left != null)
+                    queue.offer(curr.left);
+                if (curr.right != null)
+                    queue.offer(curr.right);
+            }
+            result++;
+        }
+
+        return result;
+    }
+}
