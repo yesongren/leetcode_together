0c95661dd0cdc210943afb09a4265a67d44e75c9
yesongren <youshouninn@gmail.com>
2020-03-08 03:11:45 +0800

    AC solution #150. Evaluate Reverse Polish Notation

---
+++ src/songren/Stack/LC150_EvaluateReversePolishNotation.java
@@ -0,0 +1,28 @@
+package songren.Stack;
+
+import java.util.Stack;
+
+/**
+ * Created by yesongren on 2020/3/7
+ * 150. Evaluate Reverse Polish Notation
+ */
+public class LC150_EvaluateReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String s : tokens) {
+            if (s.equals("+")) {
+                stack.push(stack.pop() + stack.pop());
+            } else if (s.equals("-")) {
+                stack.push(-stack.pop() + stack.pop());
+            } else if (s.equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (s.equals("/")) {
+                int num1 = stack.pop();
+                stack.push(stack.pop() / num1);
+            } else {
+                stack.push(Integer.parseInt(s));
+            }
+        }
+        return stack.pop();
+    }
+}
