79bbd1bb46d2a279d163a8e345d1ca127e133e7d
Di-Gu <isgudi00@gmail.com>
2020-03-08 01:49:07 -0800

    LC 22 parentheses

--- src/didi/Backtracking/LC022_GenerateParentheses.java
+++ src/didi/Backtracking/LC022_GenerateParentheses.java
@@ -1,1 +1,27 @@
 package didi.backtracking;
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        if (n <= 0) return result;
+
+        Helper("", result, n, 0, 0);
+
+        return result;
+    }
+
+    public void Helper(String curr, List<String> res, int n, int left, int right) {
+        if (right == n) {
+            res.add(curr);
+            return;
+        }
+
+        if (left < n) {
+            Helper(curr+"(", res, n, left + 1, right);
+        }
+
+        if (right < left) {
+            Helper(curr+")", res, n, left, right + 1);
+        }
+    }
+}
