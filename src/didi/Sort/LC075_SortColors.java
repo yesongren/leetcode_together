36f3d210b5d2fbd9ac17b3e25a63c9fb66f0735c
Di-Gu <isgudi00@gmail.com>
2020-03-08 23:03:26 -0700

    LC 75 sort color (pointers)

--- src/didi/Sort/LC075_SortColors.java
+++ src/didi/Sort/LC075_SortColors.java
@@ -1,1 +1,30 @@
 package didi.sort;
+
+class Solution {
+    public void sortColors(int[] nums) {
+        if (nums.length <= 1 || nums == null) return;
+        int index = 0, first = 0, last = nums.length - 1;
+        while(index <= last && nums[index] == 0) index++;
+        while(last > 0 && nums[last] == 2) last--;
+        first = index;
+
+        while (index <= last) {
+            if (nums[index] == 0) {
+                switchOrder(nums, first, index);
+                first++;
+                index++;
+            } else if (nums[index] == 1) {
+                index++;
+            } else if (nums[index] == 2) {
+                switchOrder(nums, index, last);
+                last--;
+            }
+        }
+    }
+
+    private void switchOrder(int[] nums, int first, int last) {
+        int tmp = nums[last];
+        nums[last] = nums[first];
+        nums[first] = tmp;
+    }
+}
