ba75178ecd432dc247b1c90467da461a595da448
Di-Gu <isgudi00@gmail.com>
2020-03-09 01:01:12 -0700

    LC 147 listnode sort

--- src/didi/Sort/LC147_InsertionSortList.java
+++ src/didi/Sort/LC147_InsertionSortList.java
@@ -1,1 +1,26 @@
 package didi.sort;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(0);
+
+        while(head != null) {
+            ListNode node = dummy;
+            while(node.next != null && node.next.val <= head.val) node = node.next;
+            ListNode tmp = head.next;
+            head.next = node.next;
+            node.next = head;
+            head = tmp;
+        }
+
+        return dummy.next;
+    }
+}
