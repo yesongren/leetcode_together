529436d666729015da75e8c5b78bb0ceacf8c2ea
Di-Gu <isgudi00@gmail.com>
2020-03-10 02:07:54 -0700

    LC 190 reverse bits (shift)

--- src/didi/BitManipulation/LC190_ReverseBits.java
+++ src/didi/BitManipulation/LC190_ReverseBits.java
@@ -1,1 +1,15 @@
 package didi.bitmanipulation;
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int reversed = 0;
+
+        for (int i = 0; i < 32; i++) {
+            reversed = (reversed << 1) | (n & 1);
+            n = (n >> 1);
+        }
+
+        return reversed;
+    }
+}
