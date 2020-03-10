3f6272fee8226d359b8a7d7b677e1a35e09c8b63
Di-Gu <isgudi00@gmail.com>
2020-03-10 02:01:12 -0700

    LC 187 repeated dna (set)

--- src/didi/BitManipulation/LC187_RepeatedDNASequences.java
+++ src/didi/BitManipulation/LC187_RepeatedDNASequences.java
@@ -1,1 +1,17 @@
 package didi.bitmanipulation;
+
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set one = new HashSet();
+        Set two = new HashSet();
+
+        for (int i = 0; i < s.length() - 9; i++) {
+            String tmp = s.substring(i, i + 10);
+            if (!one.add(tmp)) {
+                two.add(tmp);
+            }
+        }
+
+        return new ArrayList(two);
+    }
+}
