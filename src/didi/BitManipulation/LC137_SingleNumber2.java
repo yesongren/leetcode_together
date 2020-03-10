639b1cd58f85cdcbacfe119e801a070b736e6b8a
Di-Gu <isgudi00@gmail.com>
2020-03-10 01:39:43 -0700

    LC 137 single number 2 (shift operator)

---
+++ src/didi/BitManipulation/LC137_SingleNumber2.java
@@ -0,0 +1,16 @@
+package didi.bitmanipulation;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+
+        for(int i = 0; i < 32; i++){
+            int sum = 0;
+            for(int n: nums) sum += ((n >> i) & 1);
+            sum %= 3;
+            res |= sum << i;
+        }
+
+        return res;
+    }
+}
