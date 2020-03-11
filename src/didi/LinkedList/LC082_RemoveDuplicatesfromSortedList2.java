febf4d02624129e28255e896be7ac48b29e1adb1
Di-Gu <isgudi00@gmail.com>
2020-03-11 11:02:53 -0700

    LC 82 remove duplicates

--- src/didi/LinkedList/LC082_RemoveDuplicatesfromSortedList2.java
+++ src/didi/LinkedList/LC082_RemoveDuplicatesfromSortedList2.java
@@ -1,1 +1,32 @@
 package didi.linkedlist;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode result = new ListNode(0);
+        result.next = head;
+        ListNode dummy = result;
+
+        while (head != null) {
+            if (head.next != null && head.val == head.next.val) {
+                while (head.next != null && head.val == head.next.val) head = head.next;
+                dummy.next = head.next;
+                head = head.next;
+            } else {
+                head = head.next;
+                dummy = dummy.next;
+            }
+        }
+
+        return result.next;
+    }
+}
