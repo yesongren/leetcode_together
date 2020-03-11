10a499bebb67a19ad74fb10989704e675b4b950e
Di-Gu <isgudi00@gmail.com>
2020-03-11 11:57:46 -0700

    LC 86 partition list

--- src/didi/LinkedList/LC086_PartitionList.java
+++ src/didi/LinkedList/LC086_PartitionList.java
@@ -1,1 +1,41 @@
 package didi.linkedlist;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        if (head == null || head.next == null) return head;
+
+        ListNode curr = head;
+        ListNode less = new ListNode(0);
+        ListNode result = less;
+        ListNode great = new ListNode(0);
+        ListNode half = great;
+
+        while (curr != null) {
+            if (curr.val < x) {
+                less.next = curr;
+                ListNode newHead = curr.next;
+                curr.next = null;
+                curr = newHead;
+                less = less.next;
+            } else {
+                great.next = curr;
+                ListNode newHead = curr.next;
+                curr.next = null;
+                curr = newHead;
+                great = great.next;
+            }
+        }
+
+        less.next = half.next;
+
+        return result.next;
+    }
+}
