0aa71a17f073cf0af2eceacf276a6caff6200636
Di-Gu <isgudi00@gmail.com>
2020-03-11 09:29:07 -0700

    LC 24 swap node in pairs

--- src/didi/LinkedList/LC024_SwapNodesinPairs.java
+++ src/didi/LinkedList/LC024_SwapNodesinPairs.java
@@ -1,1 +1,35 @@
 package didi.linkedlist;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null) return null;
+        if (head.next == null) return head;
+
+        ListNode result = new ListNode(0);
+        ListNode dummy = result;
+
+        while (head != null) {
+            ListNode tmp = head.next;
+            if (head.next == null) break;
+            ListNode nexthead = head.next.next;
+            head.next.next = null;
+            head.next = null;
+            dummy.next = tmp;
+            dummy.next.next = head;
+            dummy = dummy.next.next;
+            head = nexthead;
+        }
+
+        if (head != null) dummy.next = head;
+
+        return result.next;
+    }
+}
