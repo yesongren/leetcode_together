984365afacf170c3ae5af166bd50c39c7a70279c
Di-Gu <isgudi00@gmail.com>
2020-03-12 01:29:31 -0700

    LC 264 ugly number 2

--- src/didi/Heap/LC264_UglyNumber2.java
+++ src/didi/Heap/LC264_UglyNumber2.java
@@ -1,1 +1,26 @@
 package didi.heap;
+
+class Solution {
+    public int nthUglyNumber(int n) {
+        int[] ugly = new int[n];
+        ugly[0] = 1;
+
+        int index2 = 0, index3 = 0, index5 = 0;
+
+        int curr2 = 2, curr3 = 3, curr5 = 5;
+
+        for (int i = 1; i < n; i++) {
+            int num = Math.min(Math.min(curr2, curr3), curr5);
+            ugly[i] = num;
+
+            if (num == curr2)
+                curr2 = ugly[++index2] * 2;
+            if (num == curr3)
+                curr3 = ugly[++index3] * 3;
+            if (num == curr5)
+                curr5 = ugly[++index5] * 5;
+        }
+
+        return ugly[n - 1];
+    }
+}
