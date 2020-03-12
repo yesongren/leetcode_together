eff6c976a1df030e14fa9ff476be35a8cced3810
yesongren <youshouninn@gmail.com>
2020-03-13 02:34:16 +0800

    AC Solution #095. Unique Binary Search Trees II

--- src/songren/Tree/LC095_UniqueBinarySearchTrees2.java
+++ src/songren/Tree/LC095_UniqueBinarySearchTrees2.java
@@ -1,6 +1,7 @@
 package songren.Tree;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -9,9 +10,10 @@ import java.util.List;
  */
 public class LC095_UniqueBinarySearchTrees2 {
     public List<TreeNode> generateTrees(int n) {
-        if(n == 0) return new ArrayList<TreeNode>();
+        if (n == 0) return new ArrayList<TreeNode>();
         return genTreeList(1, n);
     }
+
     public List<TreeNode> genTreeList(int start, int end) {
         List<TreeNode> ans = new ArrayList<TreeNode>();
         if (start > end) {
@@ -34,5 +36,41 @@ public class LC095_UniqueBinarySearchTrees2 {
             }
         }
         return ans;
+    }
+
+    /**
+     * Time/Space: O(4^n / n^(3/2))
+     */
+    private TreeNode cloneTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode newRoot = new TreeNode(root.val);
+        newRoot.left = cloneTree(root.left);
+        newRoot.right = cloneTree(root.right);
+        return newRoot;
+    }
+
+    private List<TreeNode> genTrees(int low, int high) {
+        if (low > high) return Collections.singletonList(null);
+        if (low == high) return Collections.singletonList(new TreeNode(low));
+        List<TreeNode> result = new ArrayList<>();
+
+        for (int i = low; i <= high; ++i) {
+            List<TreeNode> lefts = genTrees(low, i - 1);
+            List<TreeNode> rights = genTrees(i + 1, high);
+            for (TreeNode left : lefts) {
+                for (TreeNode right : rights) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = cloneTree(left);
+                    root.right = cloneTree(right);
+                    result.add(root);
+                }
+            }
+        }
+        return result;
+    }
+
+    public List<TreeNode> generateTrees2(int n) {
+        if (n < 1) return Collections.emptyList();
+        return genTrees(1, n);
     }
 }
