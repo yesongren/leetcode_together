23f8ccf3d3224aa428421c6adb7bf1dd78a4e12b
Di-Gu <isgudi00@gmail.com>
2020-03-22 03:42:35 -0700

    LC 424 longest repeating character

---
+++ src/didi/SlidingWindow/LC424_LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,24 @@
+package didi.slidingwindow;
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+        int left = 0;
+        int result = 0;
+        int mostFreq = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            mostFreq = Math.max(mostFreq, ++freq[c - 'A']);
+            int replace = right - left + 1 - mostFreq;
+            if (replace > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            } else {
+                result = Math.max(result, right - left + 1);
+            }
+        }
+
+        return result;
+    }
+}
