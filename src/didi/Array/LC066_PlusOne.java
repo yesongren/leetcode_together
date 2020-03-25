867a5a072926fb86e326cfb403fe1bdeb9e407a1
Di-Gu <isgudi00@gmail.com>
2020-03-24 21:35:23 -0700

    LC 66 plus one

---
+++ src/didi/Array/LC066_PlusOne.java
@@ -0,0 +1,23 @@
+package didi.array;
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        if(digits == null || digits.length == 0) return digits;
+
+        int n = digits.length;
+        int idx = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i] = digits[i] + 1;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+
+        int[] res = new int[n + 1];
+        res[0] = 1;
+        return res;
+    }
+}
