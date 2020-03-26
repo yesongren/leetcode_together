2581b18274a213c5cdabad8528d871910b1bfc7e
Di-Gu <isgudi00@gmail.com>
2020-03-26 02:22:29 -0700

    LC 121 best time to buy and sell stock

--- src/didi/02_DynamicProgramming/LC121_BestTimetoBuyandSellStock.java
+++ src/didi/02_DynamicProgramming/LC121_BestTimetoBuyandSellStock.java
@@ -1,1 +1,13 @@
 package didi.dp;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int res = 0;
+        for (int i : prices) {
+            min = Math.min(i,min);
+            res = Math.max(res, i - min);
+        }
+        return res;
+    }
+}
