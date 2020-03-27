ea2ae8927f23f016aa1d498ab65270e849f5eb71
Di-Gu <isgudi00@gmail.com>
2020-03-26 22:57:34 -0700

    LC 69 sqrt x

--- src/didi/03_Math/LC069_Sqrtx.java
+++ src/didi/03_Math/LC069_Sqrtx.java
@@ -1,1 +1,19 @@
 package didi.math;
+
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+
+        int left = 1;
+        int right = x;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid && (mid + 1) > x / (mid + 1)) return mid;
+            else if (mid > x / mid) right = mid;
+            else left = mid;
+        }
+
+        return -1;
+    }
+}
