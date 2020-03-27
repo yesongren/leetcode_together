9a0686879c33484c80262f52a20fe554c7d4122e
Di-Gu <isgudi00@gmail.com>
2020-03-26 23:13:59 -0700

    LC 168 excel sheet column

--- src/didi/03_Math/LC168_ExcelSheetColumnTitle.java
+++ src/didi/03_Math/LC168_ExcelSheetColumnTitle.java
@@ -1,1 +1,16 @@
 package didi.math;
+
+class Solution {
+    public String convertToTitle(int n) {
+        int x = n;
+        StringBuilder res = new StringBuilder();
+
+        while (x > 0) {
+            x--;
+            res.append((char)(x % 26 + 'A'));
+            x /= 26;
+        }
+
+        return res.reverse().toString();
+    }
+}
