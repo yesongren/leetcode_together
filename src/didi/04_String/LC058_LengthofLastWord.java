77da9200cdc94b5ec353f60bb011e21908c67e01
Di-Gu <isgudi00@gmail.com>
2020-03-27 19:06:13 -0700

    LC 58 length of last world

--- src/didi/04_String/LC058_LengthofLastWord.java
+++ src/didi/04_String/LC058_LengthofLastWord.java
@@ -1,1 +1,19 @@
 package didi.string;
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (count == 0) {
+                if (s.charAt(i) == ' ') continue;
+                else count = 1;
+            } else {
+                if (s.charAt(i) == ' ') break;
+                else count++;
+            }
+        }
+
+        return count;
+    }
+}
