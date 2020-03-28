96b4307aefa05e2b594a62a68977405720d004a6
Di-Gu <isgudi00@gmail.com>
2020-03-27 19:28:58 -0700

    LC 125 valid palindrome

--- src/didi/04_String/LC125_ValidPalindrome.java
+++ src/didi/04_String/LC125_ValidPalindrome.java
@@ -1,1 +1,31 @@
 package didi.string;
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) return true;
+
+        String str = s.toLowerCase();
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            while (left < str.length() && !isValid(str.charAt(left))) left++;
+            if (left == str.length()) return true;
+
+            while (right >= 0 && !isValid(str.charAt(right))) right--;
+
+            if (str.charAt(left) != str.charAt(right)) return false;
+            else {
+                left++;
+                right--;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isValid (char c) {
+        return Character.isLetter(c) || Character.isDigit(c);
+    }
+}
