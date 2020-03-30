76d977c14519a24b117a8502c4716e226c8ea5f3
Di-Gu <isgudi00@gmail.com>
2020-03-30 02:56:07 -0700

    LC 114 flatten binary tree

--- src/didi/05_Tree/LC114_FlattenBinaryTreetoLinkedList.java
+++ src/didi/05_Tree/LC114_FlattenBinaryTreetoLinkedList.java
@@ -1,1 +1,56 @@
 package didi.Tree;
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private TreeNode lastNode = null;
+
+    public void flatten1(TreeNode root) {
+        if (root == null) return;
+
+        if (lastNode != null) {
+            lastNode.left = null;
+            lastNode.right = root;
+        }
+
+        lastNode = root;
+        TreeNode right = root.right;
+        flatten(root.left);
+        flatten(right);
+    }
+
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.empty()) {
+            TreeNode node = stack.pop();
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+
+            // connect
+            node.left = null;
+            if (stack.empty()) {
+                node.right = null;
+            } else {
+                node.right = stack.peek();
+            }
+        }
+    }
+}
