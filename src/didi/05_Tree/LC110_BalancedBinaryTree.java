ed680b10c56e65356e625d076f7954261965c65f
Di-Gu <isgudi00@gmail.com>
2020-03-30 00:45:45 -0700

    LC 110 balance binary tree

--- src/didi/05_Tree/LC110_BalancedBinaryTree.java
+++ src/didi/05_Tree/LC110_BalancedBinaryTree.java
@@ -1,1 +1,25 @@
 package didi.Tree;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        return height(root) != -1;
+    }
+
+    private int height(TreeNode root) {
+        if (root == null) return 0;
+        int left = height(root.left);
+        int right = height(root.right);
+        if (left == -1 || right == -1 || Math.abs(left-right) > 1) return -1;
+        return Math.max(left, right) + 1;
+    }
+}
