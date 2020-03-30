d36cf9e11b677671bb2b9a7ab01787b2cf3b79d6
yesongren <youshouninn@gmail.com>
2020-03-31 04:13:09 +0800

    AC Solution #205. Isomorphic Strings

---
+++ src/songren/HashTable/LC205_IsomorphicStrings.java
@@ -0,0 +1,19 @@
+package songren.HashTable;
+
+/**
+ * Created by yesongren on 2020/3/31
+ * 205. Isomorphic Strings
+ */
+public class LC205_IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        char[] ch1 = s.toCharArray();
+        char[] ch2 = t.toCharArray();
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            if (s.indexOf(ch1[i]) != t.indexOf(ch2[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
