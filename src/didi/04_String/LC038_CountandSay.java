e7c632fe234835ad9bece014b96d55ba8d7f7680
Di-Gu <isgudi00@gmail.com>
2020-03-29 21:47:40 -0700

    LC 38 count and say

--- src/didi/04_String/LC038_CountandSay.java
+++ src/didi/04_String/LC038_CountandSay.java
@@ -1,1 +1,24 @@
 package didi.string;
+
+class Solution {
+    public String countAndSay(int n) {
+        if (n <= 0) return "";
+        String str = "1";
+        for (int i = 1; i < n; i++) {
+            int count = 0;
+            char prev = '.';
+            StringBuilder sb = new StringBuilder();
+            for (int idx = 0; idx < str.length(); idx++) {
+                if (str.charAt(idx) == prev || prev == '.') count++;
+                else {
+                    sb.append(count + Character.toString(prev));
+                    count = 1;
+                }
+                prev = str.charAt(idx);
+            }
+            sb.append(count + Character.toString(prev));
+            str = sb.toString();
+        }
+        return str;
+    }
+}
