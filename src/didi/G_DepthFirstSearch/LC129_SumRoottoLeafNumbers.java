e16001eaf5e22f52dd4cbf079f1e82c367f110a0
Di-Gu <isgudi00@gmail.com>
2020-03-31 23:53:23 -0700

    LC 129 sum root to leaf

--- src/didi/G_DepthFirstSearch/LC129_SumRoottoLeafNumbers.java
+++ src/didi/G_DepthFirstSearch/LC129_SumRoottoLeafNumbers.java
@@ -1,4 +1,13 @@
 package src.didi.G_DepthFirstSearch;
 
 public class LC129_SumRoottoLeafNumbers {
+    public int sumNumbers(TreeNode root) {
+        return helper(root, 0);
+    }
+    private int helper(TreeNode root, int prev) {
+        if (root == null) return 0;
+        int tmp = prev * 10 + root.val;
+        if (root.left == null && root.right == null) return tmp;
+        return helper(root.left, tmp) + helper(root.right, tmp);
+    }
 }
