7f3c5e39bbbcb43057e26e774a72bc41e0e0476c
Di-Gu <isgudi00@gmail.com>
2020-04-01 00:12:41 -0700

    LC 199 binary tree (bfs)

--- src/didi/G_DepthFirstSearch/LC199_BinaryTreeRightSideView.java
+++ src/didi/G_DepthFirstSearch/LC199_BinaryTreeRightSideView.java
@@ -1,4 +1,33 @@
 package src.didi.G_DepthFirstSearch;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class LC199_BinaryTreeRightSideView {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+                if (i == size - 1) res.add(curr.val);
+                if (curr.left != null) q.offer(curr.left);
+                if (curr.right != null) q.offer(curr.right);
+            }
+        }
+
+        return res;
+    }
 }
