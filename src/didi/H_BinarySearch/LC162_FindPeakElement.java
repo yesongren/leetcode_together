86e5d373fbe6a798554ca7b65e094057411f8e95
Di-Gu <isgudi00@gmail.com>
2020-04-08 02:52:06 -0700

    LC 162 find peak element

--- src/didi/H_BinarySearch/LC162_FindPeakElement.java
+++ src/didi/H_BinarySearch/LC162_FindPeakElement.java
@@ -3,6 +3,7 @@ package src.didi.H_BinarySearch;
 public class LC162_FindPeakElement {
     public int findPeakElement(int[] nums) {
         int l = 0, r = nums.length - 1;
+
         while (l < r) {
             int mid = l + (r - l) / 2;
             if (nums[mid] < nums[mid + 1]) l = mid + 1;
