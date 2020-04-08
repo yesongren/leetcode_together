65100bc0d1c0bea5d25e8378f3d759b132e26e79
Di-Gu <isgudi00@gmail.com>
2020-04-08 01:05:09 -0700

    LC 167 two sum 2

--- src/didi/H_BinarySearch/LC167_TowSum2.java
+++ src/didi/H_BinarySearch/LC167_TowSum2.java
@@ -3,6 +3,7 @@ package src.didi.H_BinarySearch;
 public class LC167_TowSum2 {
     public int[] twoSum(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
+
         while(left <= right) {
             if (nums[left] + nums[right] < target) left++;
             else if (nums[left] + nums[right] > target) right--;
