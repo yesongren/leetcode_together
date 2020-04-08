359f90fe62ca51a3a8e461e7646a90ea6036f8e5
Di-Gu <isgudi00@gmail.com>
2020-04-08 02:39:57 -0700

    LC 167 two sum 2

--- src/didi/H_BinarySearch/LC081_SearchinRotatedSortedArray2.java
+++ src/didi/H_BinarySearch/LC081_SearchinRotatedSortedArray2.java
@@ -1,4 +1,38 @@
 package src.didi.H_BinarySearch;
 
 public class LC081_SearchinRotatedSortedArray2 {
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return false;
+
+        int l = 0, r = nums.length - 1;
+
+        while (l <= r)
+        {
+            while (l < r && nums[l] == nums[l + 1]) l++;
+            while (r > l && nums[r] == nums[r - 1]) r--;
+
+            int mid = l + (r - l) / 2;
+
+            if (target == nums[mid]) return true;
+
+            if (nums[mid] > nums[r]) {
+                if (target < nums[mid] && target >= nums[l])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            } else if (nums[mid] < nums[l]) {
+                if (target > nums[mid] && target <= nums[r])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            } else {
+                if (target < nums[mid])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+        }
+
+        return false;
+    }
 }
