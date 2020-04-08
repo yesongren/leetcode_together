0bfdbf344a42253b60cd3ef44a934e368f24f0a9
Di-Gu <isgudi00@gmail.com>
2020-04-08 02:47:32 -0700

    LC 153 find min in rotated array

--- src/didi/H_BinarySearch/LC153_FindMinimumInRotatedSortedArray.java
+++ src/didi/H_BinarySearch/LC153_FindMinimumInRotatedSortedArray.java
@@ -1,4 +1,15 @@
 package src.didi.H_BinarySearch;
 
 public class LC153_FindMinimumInRotatedSortedArray {
+    public int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] > nums[r]) l = mid + 1;
+            else r = mid;
+        }
+
+        return nums[l];
+    }
 }
