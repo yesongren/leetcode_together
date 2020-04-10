5613e9828ddca798ed6a680354d7b5e76496a285
Di-Gu <isgudi00@gmail.com>
2020-04-09 22:07:07 -0700

    LC 402 remove k digits

--- src/didi/I_Greedy/LC402_RemoveKDigits.java
+++ src/didi/I_Greedy/LC402_RemoveKDigits.java
@@ -1,4 +1,22 @@
 package src.didi.I_Greedy;
 
 public class LC402_RemoveKDigits {
+    public String removeKdigits(String num, int k) {
+        int len = num.length() - k;
+        char[] res = new char[num.length()];
+        int size = 0;
+
+        for (char c : num.toCharArray()) {
+            while (size > 0 && res[size - 1] > c && k > 0) {
+                size--;
+                k--;
+            }
+            res[size++] = c;
+        }
+
+        int start = 0;
+        while (start < len && res[start] == '0') start++;
+
+        return start == len ? "0" : new String(res, start, len - start);
+    }
 }
