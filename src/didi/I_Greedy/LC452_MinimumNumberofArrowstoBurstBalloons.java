59e6fe7d2a3c823587ccc142f742749492657c61
Di-Gu <isgudi00@gmail.com>
2020-04-10 01:36:49 -0700

    LC 452 minimum # of arrows to shot

--- src/didi/I_Greedy/LC452_MinimumNumberofArrowstoBurstBalloons.java
+++ src/didi/I_Greedy/LC452_MinimumNumberofArrowstoBurstBalloons.java
@@ -1,4 +1,23 @@
 package src.didi.I_Greedy;
 
+import java.util.Arrays;
+
 public class LC452_MinimumNumberofArrowstoBurstBalloons {
+    class Solution {
+        public int findMinArrowShots(int[][] points) {
+            if (points == null || points.length == 0 || points[0].length == 0) return 0;
+
+            Arrays.sort(points, (a, b) -> (a[1] == b[1])? a[0] - b[0] : a[1] - b[1]);
+            int count = 1;
+            int[] prev = points[0];
+
+            for (int i = 1; i < points.length; i++) {
+                if (prev[1] >= points[i][0]) continue;
+                count++;
+                prev = points[i];
+            }
+
+            return count;
+        }
+    }
 }
