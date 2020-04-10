f638c3b34fca2a91d06ca892356c186ecb58bc79
Di-Gu <isgudi00@gmail.com>
2020-04-10 01:13:41 -0700

    LC 435 non-overlapping intervals

--- src/didi/I_Greedy/LC435_NonoverlappingIntervals.java
+++ src/didi/I_Greedy/LC435_NonoverlappingIntervals.java
@@ -1,4 +1,21 @@
 package src.didi.I_Greedy;
 
+import java.util.Arrays;
+
 public class LC435_NonoverlappingIntervals {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals == null || intervals.length == 0 || intervals[0].length == 0) return 0;
+        Arrays.sort(intervals, (a, b) -> (a[1] == b[1])? a[0] - b[0] : a[1] - b[1]);
+        int res = 0;
+        int[] prev = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] >= prev[1]) {
+                prev = intervals[i];
+            } else {
+                res++;
+                prev = prev[1] <= intervals[i][1] ? prev : intervals[i];
+            }
+        }
+        return res;
+    }
 }
