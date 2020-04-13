1bdc6f17d131114f9aa2ede95bc15a603b585f97
Di-Gu <isgudi00@gmail.com>
2020-04-13 01:00:39 -0700

    LC 141 linkedlist cycle

--- src/didi/K_TwoPointers/LC141_LinkedListCycle.java
+++ src/didi/K_TwoPointers/LC141_LinkedListCycle.java
@@ -1,4 +1,28 @@
 package src.didi.K_TwoPointers;
 
 public class LC141_LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (slow != fast) {
+            if (fast == null || fast.next == null) return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
+    }
 }
+
+ class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) {
+         val = x;
+         next = null;
+     }
+ }
+
