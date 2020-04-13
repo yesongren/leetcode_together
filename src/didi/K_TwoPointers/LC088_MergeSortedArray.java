8cc77c05a9b54aca0147bf6e2367b2461a6ea978
Di-Gu <isgudi00@gmail.com>
2020-04-13 00:48:04 -0700

    LC 88 merge two arrays

--- src/didi/K_TwoPointers/LC088_MergeSortedArray.java
+++ src/didi/K_TwoPointers/LC088_MergeSortedArray.java
@@ -1,4 +1,16 @@
 package src.didi.K_TwoPointers;
 
 public class LC088_MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, len = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) nums1[len--] = nums1[i--];
+            else nums1[len--] = nums2[j--];
+        }
+
+        while (j >= 0) {
+            nums1[len--] = nums2[j--];
+        }
+    }
 }
