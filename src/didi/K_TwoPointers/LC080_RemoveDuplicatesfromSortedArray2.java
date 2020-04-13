70f1b267a5333e58eeeefcffce12894a337fab52
Di-Gu <isgudi00@gmail.com>
2020-04-13 03:16:58 -0700

    LC 80 remove duplicates 2

--- src/didi/K_TwoPointers/LC080_RemoveDuplicatesfromSortedArray2.java
+++ src/didi/K_TwoPointers/LC080_RemoveDuplicatesfromSortedArray2.java
@@ -1,4 +1,14 @@
 package src.didi.K_TwoPointers;
 
 public class LC080_RemoveDuplicatesfromSortedArray2 {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int i = 1;
+        for (int j = 2; j < nums.length; j++) {
+            if (nums[i - 1] != nums[j]) {
+                nums[++i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
 }
