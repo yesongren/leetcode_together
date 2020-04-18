d52db0a550fbd861f9ada3412913eea185110933
Di-Gu <isgudi00@gmail.com>
2020-04-17 19:16:28 -0700

    LC 48 rotate image

--- src/didi/A_Array/LC048_RotateImage.java
+++ src/didi/A_Array/LC048_RotateImage.java
@@ -1,11 +1,12 @@
-package didi.array;
+package src.didi.A_Array;
 
-class Solution {
+class LC048_RotateImage {
     public void rotate(int[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return;
 
         int n = matrix.length;
 
+        // 上下互换
         for (int row = 0; row < n / 2; row++) {
             for (int col = 0; col < n; col++) {
                 int tmp = matrix[row][col];
@@ -14,6 +15,7 @@ class Solution {
             }
         }
 
+        // 对角互换
         for (int row = 0; row < n; row++) {
             for (int col = row; col < n; col++) {
                 int tmp = matrix[row][col];
