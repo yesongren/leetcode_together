7b80c036e63d862c55a449bfb799fe8bed584469
Di-Gu <isgudi00@gmail.com>
2020-04-21 20:53:18 -0700

    LC 62 unique paths

--- src/didi/B_DynamicProgramming/LC062_UniquePaths.java
+++ src/didi/B_DynamicProgramming/LC062_UniquePaths.java
@@ -1,15 +1,19 @@
-package didi.dp;
+package src.didi.B_DynamicProgramming;
 
 class Solution {
-    public int uniquePaths(int m, int n) {
+    public int LC062_UniquePaths(int m, int n) {
         if(m == 0||n == 0) return 0;
         if(m == 1||n == 1) return 1;
-        int[] dp = new int[n];
-        for(int i = 0; i < n; i++) dp[i] = 1;
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++)
-                dp[j] += dp[j-1];
+        int[][] dp = new int[n][m];
+
+        for (int i = 0; i < n; i++) dp[i][0] = 1;
+        for (int i = 0; i < m; i++) dp[0][i] = 1;
+
+        for (int i = 1; i < n; i++) {
+            for(int j = 1; j < m; j++)
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
         }
-        return dp[n-1];
+
+        return dp[n - 1][m - 1];
     }
 }
