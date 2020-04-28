f4ec95e87da149eeb35920552c14d2c6732f65f9
Di-Gu <isgudi00@gmail.com>
2020-04-27 17:36:40 -0700

    LC 1208 get equal substring within budget

--- src/didi/T_SlidingWindow/LC1208_GetEqualSubstringsWithinBudget.java
+++ src/didi/T_SlidingWindow/LC1208_GetEqualSubstringsWithinBudget.java
@@ -1,4 +1,26 @@
 package src.didi.T_SlidingWindow;
 
 public class LC1208_GetEqualSubstringsWithinBudget {
+    public int equalSubstring(String s, String t, int maxCost) {
+        int n = s.length();
+        int res = 0;
+        int[] diffs = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            diffs[i] = Math.abs(s.charAt(i) - t.charAt(i));
+        }
+
+        for (int r = 0, l = 0, current = 0; r < n; r++) {
+            current += diffs[r];
+
+            while (current > maxCost) {
+                current -= diffs[l];
+                l++;
+            }
+
+            res = Math.max(res, r -l + 1);
+        }
+
+        return res;
+    }
 }
---
+++ src/didi/T_SlidingWindow/LC1423_MaximumPointsYouCanObtainFromCards.java
@@ -0,0 +1,4 @@
+package src.didi.T_SlidingWindow;
+
+public class LC1423_MaximumPointsYouCanObtainFromCards {
+}
