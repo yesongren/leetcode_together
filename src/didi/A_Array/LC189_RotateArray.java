4d76aa93f61c1828da12afa888394bfa8ac31d4a
Di-Gu <isgudi00@gmail.com>
2020-04-30 01:08:19 -0700

    LC 189 rotate array

--- src/didi/A_Array/LC189_RotateArray.java
+++ src/didi/A_Array/LC189_RotateArray.java
@@ -1,4 +1,19 @@
 package src.didi.A_Array;
 
 public class LC189_RotateArray {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+        reverse(nums, 0, n - k - 1);
+        reverse(nums, n - k, n - 1);
+        reverse(nums, 0, n - 1);
+    }
+
+    public void reverse(int[] n, int i, int j) {
+        for (int p = i, q = j; p < q; p++, q--) {
+            int temp = n[p];
+            n[p] = n[q];
+            n[q] = temp;
+        }
+    }
 }
