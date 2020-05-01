8b845e4083c8f3ff3089dcbe0080983a698e7ea9
Di-Gu <isgudi00@gmail.com>
2020-04-30 18:44:30 -0700

    LC 303 range sum query

---
+++ src/didi/B_DynamicProgramming/LC303_RangeSumQuery.java
@@ -0,0 +1,15 @@
+package src.didi.B_DynamicProgramming;
+
+public class LC303_RangeSumQuery {
+    private int[] sum;
+    public LC303_RangeSumQuery(int[] nums) {
+        sum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sum[j + 1] - sum[i];
+    }
+}
