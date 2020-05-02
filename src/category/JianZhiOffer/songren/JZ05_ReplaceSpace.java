8d45f5841ab85337b69240bdb32914dfdb15febe
yesongren <youshouninn@gmail.com>
2020-05-03 00:09:49 +0800

    AC Solution: 面试题05. 替换空格

--- src/category/JianZhiOffer/songren/JZ05_ReplaceSpace.java
+++ src/category/JianZhiOffer/songren/JZ05_ReplaceSpace.java
@@ -5,4 +5,22 @@ package category.JianZhiOffer.songren;
  * 面试题05. 替换空格
  */
 public class JZ05_ReplaceSpace {
+
+    public String replaceSpace(String s) {
+        char[] str = s.toCharArray();
+        StringBuilder ans = new StringBuilder();
+        for (char c : str) {
+            if (c == ' ')
+                ans.append("%20");
+            else
+                ans.append(c);
+        }
+        return ans.toString();
+    }
+
+    // Use "replace" func in String class
+    public String replaceSpace2(String s) {
+        s = s.replace(" ", "%20");
+        return s;
+    }
 }
