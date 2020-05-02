50fe3d54512ff5ab7558b24c8eb064ea70b8c772
yesongren <youshouninn@gmail.com>
2020-05-02 23:25:15 +0800

    AC Solution: 面试题04. 二维数组中的查找

--- src/category/JianZhiOffer/songren/JZ04_FindNumberIn2DArray.java
+++ src/category/JianZhiOffer/songren/JZ04_FindNumberIn2DArray.java
@@ -5,4 +5,15 @@ package category.JianZhiOffer.songren;
  * 面试题04. 二维数组中的查找
  */
 public class JZ04_FindNumberIn2DArray {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+        int n = matrix.length, m = matrix[0].length;
+        int i = 0, j = m - 1;
+        while (i < n && j >= 0) {
+            if (target < matrix[i][j]) j--;
+            else if (target > matrix[i][j]) i++;
+            else return true;
+        }
+        return false;
+    }
 }
