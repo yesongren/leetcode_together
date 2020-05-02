10bfed6c4b3e627caf1f4c5f2d78fdbc2e165c70
yesongren <youshouninn@gmail.com>
2020-05-02 18:37:24 +0800

    AC Solution: 面试题03. 数组中重复的数字

--- src/category/JianZhiOffer/songren/JZ03_FindRepeatNumber.java
+++ src/category/JianZhiOffer/songren/JZ03_FindRepeatNumber.java
@@ -1,8 +1,31 @@
 package category.JianZhiOffer.songren;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by yesongren on 2020/5/2
  * 面试题03. 数组中重复的数字
  */
 public class JZ03_FindRepeatNumber {
+
+    // HashSet
+    public int findRepeatNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) return num;
+        }
+        return -1;
+    }
+
+
+    // Use Array instead of HashSet
+    public int findRepeatNumber2(int[] nums) {
+        int[] cnt = new int[nums.length];
+        for (int index : nums) {
+            if (cnt[index] == 1) return index;
+            cnt[index] = 1;
+        }
+        return -1;
+    }
 }
