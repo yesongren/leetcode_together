ae8b939bd9961046f5c08cea5856d8fbe3a91c13
Di-Gu <isgudi00@gmail.com>
2020-05-02 23:35:24 -0700

    面试题05. 替换空格

--- src/category/JianZhiOffer/didi/JZ05_ReplaceSpace.java
+++ src/category/JianZhiOffer/didi/JZ05_ReplaceSpace.java
@@ -1,4 +1,16 @@
 package src.category.JianZhiOffer.didi;
 
 public class JZ05_ReplaceSpace {
+    public String replaceSpace(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c != ' ') sb.append(c);
+            else sb.append("%20");
+        }
+
+        return sb.toString();
+    }
+
+    //return s.replaceAll(" ", "%20");
 }
