8f54583384e59af551a46ce7a32d16454145a092
Di-Gu <isgudi00@gmail.com>
2020-05-02 23:51:55 -0700

    面试题06. 从尾到头打印链表

--- src/category/JianZhiOffer/didi/JZ06_ReversePrint.java
+++ src/category/JianZhiOffer/didi/JZ06_ReversePrint.java
@@ -1,4 +1,33 @@
 package src.category.JianZhiOffer.didi;
 
 public class JZ06_ReversePrint {
+    public int[] reversePrint(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        int count = 0;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+            count++;
+        }
+
+        int[] res = new int[count];
+        int i = 0;
+
+        while (pre != null) {
+            res[i++] = pre.val;
+            pre = pre.next;
+        }
+
+        return res;
+    }
 }
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+ }
