8ad1c873b3eeae3fef19d4ff5b077cdddb7b1153
Di-Gu <isgudi00@gmail.com>
2020-05-02 23:28:36 -0700

    面试题04. 二维数组中的查找

--- src/category/JianZhiOffer/didi/JZ04_FindNumberIn2DArray.java
+++ src/category/JianZhiOffer/didi/JZ04_FindNumberIn2DArray.java
@@ -1,4 +1,17 @@
 package src.category.JianZhiOffer.didi;
 
 public class JZ04_FindNumberIn2DArray {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) return false;
+        int row = matrix.length, col = matrix[0].length;
+        int r = 0, c = col - 1;
+
+        while (r < row && c >= 0) {
+            if (matrix[r][c] < target) r++;
+            else if (matrix[r][c] > target) c--;
+            else return true;
+        }
+
+        return false;
+    }
 }
