e9e0bac08049e5fb2ef90066f446678bf2afb8d8
Di-Gu <isgudi00@gmail.com>
2020-05-02 23:28:58 -0700

    面试题03. 数组中重复的数字

--- src/category/JianZhiOffer/didi/JZ03_FindRepeatNumber.java
+++ src/category/JianZhiOffer/didi/JZ03_FindRepeatNumber.java
@@ -1,4 +1,14 @@
 package src.category.JianZhiOffer.didi;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class JZ03_FindRepeatNumber {
+    public int findRepeatNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            if (!set.add(i)) return i;
+        }
+        return -1;
+    }
 }
