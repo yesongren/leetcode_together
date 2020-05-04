ef947fba5e41306dd24832db5decdf42adedea07
Di-Gu <isgudi00@gmail.com>
2020-05-03 23:15:28 -0700

    面试题11. 旋转数组的最小数字

--- src/category/JianZhiOffer/didi/JZ11_MinArray.java
+++ src/category/JianZhiOffer/didi/JZ11_MinArray.java
@@ -1,4 +1,21 @@
 package src.category.JianZhiOffer.didi;
 
 public class JZ11_MinArray {
+    public int minArray(int[] a) {
+        int l = 0, r = a.length - 1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (a[mid] < a[r]) r = mid;
+            else if (a[mid] > a[r]) l = mid + 1;
+            else {
+                if (r != 0 && a[r] >= a[r - 1])
+                    r--;
+                else
+                    return a[r];
+            }
+        }
+
+        return a[r];
+    }
 }
