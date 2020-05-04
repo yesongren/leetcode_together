d33477e9152482b27730195b1c1310335fc0bef1
Di-Gu <isgudi00@gmail.com>
2020-05-03 21:42:17 -0700

    面试题10- II. 青蛙跳台阶问题

--- src/category/JianZhiOffer/didi/JZ10_2_NumWays.java
+++ src/category/JianZhiOffer/didi/JZ10_2_NumWays.java
@@ -1,4 +1,16 @@
 package src.category.JianZhiOffer.didi;
 
 public class JZ10_2_NumWays {
+    public int numWays(int n) {
+        if (n == 0) return 1;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+
+        return dp[n];
+    }
 }
