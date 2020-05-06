bcb1ec5396fa80922486bafeb0b57f176156c901
yesongren <youshouninn@gmail.com>
2020-05-07 02:12:46 +0800

    AC Solution: 面试题25. 合并两个排序的链表

---
+++ src/category/JianZhiOffer/songren/JZ25_MergeTwoLists.java
@@ -0,0 +1,24 @@
+package category.JianZhiOffer.songren;
+
+/**
+ * Created by yesongren on 2020/5/7
+ * 面试题25. 合并两个排序的链表
+ */
+public class JZ25_MergeTwoLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 != null ? l1 : l2;
+        return dummy.next;
+    }
+}
