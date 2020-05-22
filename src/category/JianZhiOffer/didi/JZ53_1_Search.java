490168d2e9effbe269ab753ddf5030929f60169c
Di-Gu <isgudi00@gmail.com>
2020-05-21 17:26:52 -0700

    面试题53 - I. 在排序数组中查找数字 I

---
+++ src/category/JianZhiOffer/didi/JZ53_1_Search.java
@@ -0,0 +1,20 @@
+package src.category.JianZhiOffer.didi;
+
+public class JZ53_1_Search {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, target + 0.5) - binarySearch(nums, target - 0.5);
+    }
+
+    private int binarySearch(int[] nums, double target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
