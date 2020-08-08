4c2e1e967b579b867efdf0dc2a97c8117726f760
yesongren <youshouninn@gmail.com>
2020-08-08 23:55:44 +0800

    init common class-TreeNode

---
+++ src/category/Daily_Challenge/songren/util/TreeNode.java
@@ -0,0 +1,12 @@
+package category.Daily_Challenge.songren.util;
+
+/**
+ * Created by yesongren on 2020/8/8
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left, right;
+    public TreeNode(int x) {
+        val = x;
+    }
+}
