20e5c7f53b5e74bd3993dc41bd596a09d0b4d26b
yesongren <youshouninn@gmail.com>
2020-08-08 17:47:05 +0800

    init commit

---
+++ src/category/Daily_Challenge/didi/Daily_Aug08.java
@@ -0,0 +1,7 @@
+package category.Daily_Challenge.didi;
+
+/**
+ * Created by yesongren on 2020/8/8
+ */
+public class Daily_Aug08 {
+}
---
+++ src/category/Daily_Challenge/songren/Daily_Aug08.java
@@ -0,0 +1,7 @@
+package category.Daily_Challenge.songren;
+
+/**
+ * Created by yesongren on 2020/8/8
+ */
+public class Daily_Aug08 {
+}
