79a35be920a5fcdae6b0178b132a82bf776fbe2a
yesongren <youshouninn@gmail.com>
2020-08-24 17:53:30 +0800

    init common class- ListNode

---
+++ src/category/Daily_Challenge/songren/util/ListNode.java
@@ -0,0 +1,12 @@
+package category.Daily_Challenge.songren.util;
+
+/**
+ * Created by yesongren on 2020/8/18
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode (int x) {
+        val = x;
+    }
+}
