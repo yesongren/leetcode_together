67cecefe524d47d72a371894bb4daf12ac512a21
Di-Gu <isgudi00@gmail.com>
2020-08-24 21:25:31 +0800

    每日一题，LC_0459

--- src/category/Daily_Challenge/didi/Daily_Aug08.java
+++
@@ -1,7 +0,0 @@
-package category.Daily_Challenge.didi;
-
-/**
- * Created by yesongren on 2020/8/8
- */
-public class Daily_Aug08 {
-}
---
+++ src/category/Daily_Challenge/didi/Daily_Aug24.java
@@ -0,0 +1,8 @@
+package src.category.Daily_Challenge.didi;
+
+public class Daily_Aug24 {
+    public boolean repeatedSubstringPattern(String s) {
+        String str = s + s;
+        return str.substring(1, str.length() - 1).contains(s);
+    }
+}
